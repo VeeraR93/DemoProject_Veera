6283ed2bf64fdb4c6a54b141f0369c4446e39971
RjK <RjK@RajKumar>
2022-08-12 16:47:22 +0530

    test commit in backup branch

--- X_DemoProject/src/demologin/Try_2_Rock.java
+++ X_DemoProject/src/demologin/Try_2_Rock.java
@@ -13,6 +13,11 @@ public class Try_2_Rock {
 	public void test2() {
 		System.out.println("do commit and push and check it is availble in git hub or not");
 	}
+	
+	@Test
+	public void test3() {
+		System.out.println("branch created in Github website - so try to push it to branch");
+	}
 
 	
 }
