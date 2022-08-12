c9977a06f75418fb5cf9ce960eff52b7a1acca84
RjK <RjK@RajKumar>
2022-08-12 16:35:25 +0530

    try with commit and push and check github

--- X_DemoProject/src/demologin/Try_2_Rock.java
+++ X_DemoProject/src/demologin/Try_2_Rock.java
@@ -1,9 +1,18 @@
 package demologin;
 
+import org.testng.annotations.Test;
+
 public class Try_2_Rock {
 	
+	@Test
 	public void test() {
 		System.out.println("nely added code to cross check in git hub");
 	}
+	
+	@Test
+	public void test2() {
+		System.out.println("do commit and push and check it is availble in git hub or not");
+	}
+
 	
 }
